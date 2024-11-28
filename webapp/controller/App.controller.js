bc542e30e944468a334aad1dc191e00483dc607f
Sneakyraccoon <zhuriko@gmail.com>
2024-11-28 21:26:50 +0200

    Input fields

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller){
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.App",{
-onShowHello : function () {
-    // Show a native or vanilla JS alert
-    alert("Hello there");
-}
+    return Controller.extend("ui5.walkthrough.controller.App", {
+
+        onInit: function () {
+            // set the data model on the view
+            let oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            let oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
+        onShowHello: function () {
+            // Show a native or vanilla JS alert
+            MessageToast.show("HELLO!!!!")
+        }
     })
 })
