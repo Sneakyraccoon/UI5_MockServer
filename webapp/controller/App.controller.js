049848e18163681a8f68017aed8f9c685f51bda2
Sneakyraccoon <zhuriko@gmail.com>
2024-11-29 20:38:23 +0200

    i18n_version

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("ui5.walkthrough.controller.App", {
 
@@ -15,11 +16,24 @@ sap.ui.define([
             };
             let oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            // set u18n model on view
+            let i18nModel = new ResourceModel({
+                bundleName: "ui5.walkthrough.i18n.i18n",
+                supportedLocales: ["en"],
+                fallbacklocale: "en"
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
 
         onShowHello: function () {
+            // read msg from i18n model
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            let sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // Show a native or vanilla JS alert
-            MessageToast.show("HELLO!!!!")
+            // MessageToast.show("HELLO!!!!")
+            // Show mwssage
+            MessageToast.show(sMsg);
         }
     })
 })
