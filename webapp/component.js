99d7d9e00d949085b176fd807ca5ceb0b166c124
Sneakyraccoon <zhuriko@gmail.com>
2024-12-06 09:44:30 +0200

    WeirCommit
    Not sure if works fine

---
+++ webapp/component.js
@@ -0,0 +1,8 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+
+], function(UIComponent, JSONModel, ResourceModel){
+    "use strict"
+})
