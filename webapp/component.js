d12501cda355cd3662594acad856927e29446ead
Sneakyraccoon <zhuriko@gmail.com>
2025-02-22 19:23:56 +0200

    final

--- webapp/component.js
+++ webapp/component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
-], (UIComponent, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+	"sap/ui/Device"
+], (UIComponent, JSONModel, Device) => {
     "use strict";
 
     return UIComponent.extend("ui5.walkthrough.Component", {
@@ -22,6 +23,21 @@ sap.ui.define([
             };
             const oModel = new JSONModel(oData);
             this.setModel(oModel);
-        }
+
+            // set device model
+			const oDeviceModel = new JSONModel(Device);
+            console.log('THIS IS DEVICEMODEL', oDeviceModel);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+            
+            // create the views based on the url/hash
+			this.getRouter().initialize();
+        },
+
+        getContentDensityClass() {
+			return Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact";
+		}
+
+
     });
 });
---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], (Controller, History, MessageToast, JSONModel) => {
+	"use strict";
+
+	return Controller.extend("ui5.walkthrough.controller.Detail", {
+		onInit() {
+
+			const oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			console.log('THIS IS OVIEWMODEL:', oViewModel);
+			this.getView().setModel(oViewModel, "view");
+
+			const oRouter = this.getOwnerComponent().getRouter();
+            //console.log('This is OROUTER', oRouter);
+            //console.log('This is THIS', this);
+			oRouter.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
+		},
+
+		onObjectMatched(oEvent) {
+			this.byId("rating").reset();
+			this.getView().bindElement({
+				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+				model: "invoice"
+			});
+		},
+
+		onNavBack() {
+			const oHistory = History.getInstance();
+			const sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				const oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
+		},
+
+		onRatingChange(oEvent) {
+			const fValue = oEvent.getParameter("value");
+			const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
+	});
+});
